be18de6df69883807ed15ef42c26a933f56072ae
James Hillyerd <james@hillyerd.com>
2019-03-31 14:35:44 -0700

    Support for running ansible on localhost

--- apply.sh
+++ apply.sh
@@ -1,3 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
+# usage:
+#   apply.sh [remote IP]
+#
+# if no remote IP is specified, will run ansible against localhost
 
-ansible-playbook --become --inventory=hosts.txt --user=pi site.yml
+inventory=$(mktemp)
+
+echo "[pi-network-lab]" > "$inventory"
+if [ "$1" == "" ]; then
+  echo "localhost ansible_connection=local" >> "$inventory"
+else
+  echo "$1" >> "$inventory"
+fi
+
+echo "### Host inventory configuration"
+cat "$inventory"
+
+ansible-playbook --become --inventory="$inventory" --user=pi site.yml
+
+rm -f "$inventory"
